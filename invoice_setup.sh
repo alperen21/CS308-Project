9a923af16d8fc29d370feffeb5a851057b3e4149
Alperen Yıldız <alperenbot086@gmail.com>
2021-05-28 21:22:03 +0300

    deleted the zip file

--- invoice_setup.sh
+++ invoice_setup.sh
@@ -2,4 +2,5 @@ pip install virtualenv
 brew install cairo
 brew install python3 cairo pango gdk-pixbuf libffi
 virtualenv project_env
+source project_env/bin/activate
 pip install -r requirements.txt

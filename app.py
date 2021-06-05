7fb73c3c80d974ac1d804cb3edd961910758696d
Alperen Yıldız <alperenbot086@gmail.com>
2021-06-05 12:48:08 +0300

    refreshed the gmail api token

--- app.py
+++ app.py
@@ -1468,7 +1468,7 @@ class order(Resource):
         
         email = get_email(customer_id)
         #send invoice
-        blob = invoice(cart_id, products_dict, email)
+        blob = invoice(cart_id, products_dict, emai)
         
         mysql.get_db().commit()
 

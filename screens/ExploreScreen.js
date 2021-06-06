38c27a5d5ec02133879b492d892da71120895556
Görkem Güzeler <gorkemguzeler@sabanciuniv.edu>
2021-06-06 12:07:50 +0300

    bugs fixed

--- screens/ExploreScreen.js
+++ screens/ExploreScreen.js
@@ -62,6 +62,15 @@ const ExploreScreen = ({ navigation }) => {
     })
     //  console.log("item geldi mi",itemname);
     let json = await response2.json();
+
+
+    if(json.status_code === 200){
+      alert("Your cart is updated")
+ 
+    }
+    else{
+      alert("Try again!")
+    }
     //console.log("mesajımız: ", json.message)
     //console.log("code: ", json.status_code)
     //setBasket(json.category_elements);  
